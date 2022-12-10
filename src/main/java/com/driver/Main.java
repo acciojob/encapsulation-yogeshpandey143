bb3dfc8a1092085f002af2e81edb973a58cf5e0b
Yogesh <bc21143@smsvaranasi.in>
2022-12-10 15:49:33 +0530

    my commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,23 @@
 package com.driver;
 
+
+
 public class Main {
+	
+	
+	public static void main(String[] args) {
+		
+		RWOnly obj= new RWOnly();
+		
+		
+	
+			
+//			obj.name="yogesh";
+		
+		
+		obj.setName("pandey");
+		System.out.println(obj.getName());
+		
+	}
   
 }
